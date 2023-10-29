ed9db4dd19875d204c084fc7d70327367bfa979e
amritpreet-singh-1 <127545921+amritpreet-singh-1@users.noreply.github.com>
2023-10-29 08:20:12 -0400

    Commit after working api pull for driver info

--- app/src/main/java/com/example/motogp/DriverInformationActivity.java
+++ app/src/main/java/com/example/motogp/DriverInformationActivity.java
@@ -4,17 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class DriverInformationActivity extends AppCompatActivity {
+
+    TextView txtFirstName, txtLastName, txtDOB, txtCountry, txtTeamName;
+    String picURL, helmetURL, bikeURL;
+
+    ImageView imageDriver, imageHelmet, imageBike;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_information);
 
-        Intent i = getIntent();
-        String receivedID = i.getStringExtra("driverID");
+        txtFirstName = findViewById(R.id.txtName);
+        txtLastName = findViewById(R.id.txtSurname);
+        txtDOB = findViewById(R.id.txtDOB);
+        txtCountry = findViewById(R.id.txtCountryName);
+        txtTeamName = findViewById(R.id.txtTeamName);
+        imageDriver = findViewById(R.id.imgDriverImage);
+        imageBike = findViewById(R.id.imgBike);
+        imageHelmet = findViewById(R.id.imgHelmet);
 
-        /* code to fetch from api and get driver's details*/
+        Intent intent = getIntent();
+
+        txtFirstName.setText(intent.getStringExtra("driverFirstName"));
+        txtLastName.setText(intent.getStringExtra("driverLastName"));
+        txtDOB.setText(intent.getStringExtra("driverDOB"));
+        txtCountry.setText(intent.getStringExtra("driverCountry"));
+        txtTeamName.setText(intent.getStringExtra("driverTeamName"));
+        picURL = intent.getStringExtra("driverPicURL");
+        helmetURL = intent.getStringExtra("driverHelmetURL");
+        bikeURL = intent.getStringExtra("driverBikeURL");
+
+        Picasso.get().load(picURL).into(imageDriver);
+        Picasso.get().load(helmetURL).into(imageHelmet);
+        Picasso.get().load(bikeURL).into(imageBike);
+
     }
 }
